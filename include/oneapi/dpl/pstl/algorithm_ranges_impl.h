1b4a9e34c5826525f02c208913741c054d0c35c4
MikeDvorskiy <mikhail.dvorskiy@intel.com>
2026-07-14 05:23:00 -0500

    Caupture __proj

--- include/oneapi/dpl/pstl/algorithm_ranges_impl.h
+++ include/oneapi/dpl/pstl/algorithm_ranges_impl.h
@@ -143,7 +143,7 @@ __pattern_find_if(__parallel_tag<_IsVector> __tag, _ExecutionPolicy&& __exec, _R
         return __parallel_find(
             __tag, std::forward<_ExecutionPolicy>(__exec), std::forward<_ExecutionPolicy>(__exec), std::ranges::begin(__r),
             std::ranges::begin(__r) + std::ranges::size(__r),
-            [__pred](auto __i, auto __j) {
+            [__pred, __proj](auto __i, auto __j) {
                 return __brick_find_if(std::ranges::subrange(__i, __j), __pred, __proj, _IsVector{});
             },
             ::std::true_type{});
